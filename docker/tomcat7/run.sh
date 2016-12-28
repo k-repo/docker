f1c2a54b391fc7de74d99e0df28065f98278d065
Azaddyne <kamal.azaddyne@gmail.com>
2016-12-28 01:30:23 +0100

    init commit

---
+++ docker/tomcat7/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f /.tomcat_admin_created ]; then
+    /create_tomcat_admin_user.sh
+fi
+
+date
+sleep 25
+date
+
+./$CATALINA_HOME/bin/startup.sh && tail -f /opt/tomcat/logs/catalina.out
